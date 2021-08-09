f81bc6721b0db4ee84b04eb9bd46d39dfd459d6d
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-08-09 15:52:37 -0400

    Corrected a bug in Question.java, it was a typo, my fault.  I wrote answer instead of answerNr.

--- app/src/main/java/com/codinginflow/myawesomequiz/Question.java
+++ app/src/main/java/com/codinginflow/myawesomequiz/Question.java
@@ -9,7 +9,7 @@ public class Question {
 
     public Question() {}
 
-    public Question(String question, String option1, String option2, String option3, int answer) {
+    public Question(String question, String option1, String option2, String option3, int answerNr) {
         this.question = question;
         this.option1 = option1;
         this.option2 = option2;
